4b63131b4adbe85a67575a2bc64e65015cdbb8e6
s0fko <faberdasss@gmail.com>
2024-08-16 15:08:16 +0300

    Edit according to pre-commit rules

--- config.py
+++ config.py
@@ -1,43 +1,45 @@
 # [GCP]
-SERVICE_ACCOUNT_CREDENTIALS = 'credentials.json'
+SERVICE_ACCOUNT_CREDENTIALS = "credentials.json"
 
 # [Google Sheets]
-SCOPE = ['https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive']
+SCOPE = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
 
-SHEET_NAME = 'sheet_name'
-ADMIN_USER_COLUMN = 'B'
+SHEET_NAME = "sheet_name"
+ADMIN_USER_COLUMN = "B"
 
 USER_NAMES_MAPPING = {
-    'John Doe1': '@telegram_nickname1',
-    'John Doe2': '@telegram_nickname2',
-    'John Doe3': '@telegram_nickname3',
-    'John Doe4': '@telegram_nickname4',
-    'John Doe5': '@telegram_nickname5',
-    'John Doe6': '@telegram_nickname6',
+    "John Doe1": "@telegram_nickname1",
+    "John Doe2": "@telegram_nickname2",
+    "John Doe3": "@telegram_nickname3",
+    "John Doe4": "@telegram_nickname4",
+    "John Doe5": "@telegram_nickname5",
+    "John Doe6": "@telegram_nickname6",
 }
 
 USER_COLUMNS_MAPPING = {
-    '@telegram_nickname1': 'B',
-    '@telegram_nickname2': 'C',
-    '@telegram_nickname3': 'D',
-    '@telegram_nickname4': 'E',
-    '@telegram_nickname5': 'F',
-    '@telegram_nickname6': 'G',
+    "@telegram_nickname1": "B",
+    "@telegram_nickname2": "C",
+    "@telegram_nickname3": "D",
+    "@telegram_nickname4": "E",
+    "@telegram_nickname5": "F",
+    "@telegram_nickname6": "G",
 }
 
 # [Telegram configs]
-BOT_TOKEN = 'bot_token'
+BOT_TOKEN = "bot_token"
 
-DESCRIPTION_PAID_UA = 'Від: '
-DESCRIPTION_WITHDRAWAL_UA = 'На чорну картку'
-DESCRIPTION_INCOME_UA = 'Додавання до банки'
+DESCRIPTION_PAID_UA = "Від: "
+DESCRIPTION_WITHDRAWAL_UA = "На чорну картку"
+DESCRIPTION_INCOME_UA = "Додавання до банки"
 
 # [Monobank configs]
-MONO_TOKEN = 'monobank_token'
-MONO_ACCOUNT = '0'
-MONOBANKA_ID = 'monobanka_id'
+MONO_TOKEN = "monobank_token"
+MONO_ACCOUNT = "0"
+MONOBANKA_ID = "monobanka_id"
 
 # [Files with stored values]
-LAST_PAY_UPDATE_FILE_NAME = 'last_pay_update.txt'
-CURRENT_ROW_TO_WRITE_FILE_NAME = 'current_row_to_write.txt'
+LAST_PAY_UPDATE_FILE_NAME = "last_pay_update.txt"
+CURRENT_ROW_TO_WRITE_FILE_NAME = "current_row_to_write.txt"
--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import gspread
+import telebot
 from gspread import Client
 from oauth2client.service_account import ServiceAccountCredentials
-import telebot
 
 import config
 
 
 def get_debtors_message(client: Client) -> str:
-    worksheet = client.open('spotify').sheet1
+    worksheet = client.open("spotify").sheet1
 
-    table = worksheet.batch_get(['C1:F2'])[0]
+    table = worksheet.batch_get(["C1:F2"])[0]
 
-    message = 'Боржники:\n'
+    message = "Боржники:\n"
     debtors_exists = False
     for i in range(len(table[0])):
-        balance = float(table[1][i].replace(',', '.'))
+        balance = float(table[1][i].replace(",", "."))
         if float(balance) < 55:
             debtors_exists = True
             # TODO: round zaplati to up int
-            message += f'{table[0][i]} твій баланс {balance}, заплати {55 - balance}\n'
+            message += f"{table[0][i]} твій баланс {balance}, заплати {55 - balance}\n"
 
     if not debtors_exists:
-        message += 'Нема :)'
+        message += "Нема :)"
     else:
-        message += '\nhttps://send.monobank.ua/jar/8DkVAacNhm'
+        message += "\nhttps://send.monobank.ua/jar/8DkVAacNhm"
 
     return message
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         config.SERVICE_ACCOUNT_CREDENTIALS,
         config.SCOPE,
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     bot = telebot.TeleBot(config.BOT_TOKEN)
 
-    @bot.message_handler(commands=['debtors'])
+    @bot.message_handler(commands=["debtors"])
     def send_debtors(message):
         bot.reply_to(message, get_debtors_message(client))
 
