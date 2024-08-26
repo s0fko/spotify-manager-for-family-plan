6f07e659493cdcd2093db0366d89b3f344e1fa2a
s0fko <faberdasss@gmail.com>
2024-08-26 21:22:54 +0300

    Add admin_only func

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import telebot
 import config
 from managers import MonoManager, SheetManager
 
+
+def admin_only(func):
+    def wrapper(message):
+        if message.from_user.id == config.ADMIN_USER_ID:
+            return func(message)
+        else:
+            bot.reply_to(message, "Команда тільки для Соні")
+
+    return wrapper
+
+
 if __name__ == "__main__":
     sheet_manager = SheetManager.SheetManager(
         credentials=config.SERVICE_ACCOUNT_CREDENTIALS,
@@ -19,10 +30,12 @@ if __name__ == "__main__":
         bot.reply_to(message, sheet_manager.get_debtors_message())
 
     @bot.message_handler(commands=["set_updates"])
+    @admin_only
     def set_updates(message):
         bot.reply_to(message, mono_manager.set_user_pay_updates())
 
     @bot.message_handler(commands=["set_charge"])
+    @admin_only
     def set_charge(message):
         bot.reply_to(message, mono_manager.set_user_charge())
 
